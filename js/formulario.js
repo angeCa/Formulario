ac31dd50da28461ae92030b6fa6bede90315c6e9
sarexRGB <velez99sara0106@gmail.com>
2025-08-07 10:00:47 -0600

    agrego javaScript

---
+++ js/formulario.js
@@ -0,0 +1,5 @@
+const producto = document.getElementById("Nproducto");
+const precio = document.getElementById("precios");
+const categoria = document.getElementById("sele1");
+const descripcion = document.getElementById("dtex");
+
